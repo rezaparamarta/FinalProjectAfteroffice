fe9718a6a0d084b9352647118de82c86b6bbacf5
rezaparamarta <rezaparamarta30@gmail.com>
2025-06-02 20:23:48 +0700

    update

--- cypress/support/pages/amazon/searchItemPage.js
+++ cypress/support/pages/amazon/searchItemPage.js
@@ -7,11 +7,11 @@ class filterItemPage {
     sortFeatureItems() {
         cy.get('#a-autoid-0-announce').click();
         cy.xpath(`//div[@class="a-popover-inner"]//li[3]`).click();
-        cy.xpath(`//div[@class="s-main-slot s-result-list s-search-results sg-row"]//div[@role="listitem"][5]`).click();
+        cy.xpath(`//div[@class="s-main-slot s-result-list s-search-results sg-row"]//div[@role="listitem"][@data-index="7"]`).click();
     }
 
     assertionPage() {
-        cy.xpath(`//h1[@id="title"]//span[@id="productTitle"]`).should('be.visible').should('have.text', 'Office Chair Recliner Leather Ergonomic Manager and Executive Office Chair(Black)');
+        cy.xpath(`//h1[@id="title"]//span[@id="productTitle"]`).should('be.visible');
         cy.xpath(`//div[@id="corePrice_feature_div"]//div[@data-csa-c-type="widget"]//span[@class="a-price-whole"]`).should('be.visible');
     }
 }
