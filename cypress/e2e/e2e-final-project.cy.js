6ee585ca03464cc2c8c48f7353d1d63b51ab0fdf
rezaparamarta <rezaparamarta30@gmail.com>
2025-06-03 19:20:01 +0700

    add code agoda

--- cypress/e2e/e2e-final-project.cy.js
+++ cypress/e2e/e2e-final-project.cy.js
@@ -7,61 +7,62 @@ describe('Final Project', () => {
         cy.clearLocalStorage();
     });
 
-    it('youtube search', () => {
-        cy.visit(Cypress.env(`BASE_URL_YOUTUBE`));
-        cy.wait(4000);
-        trendingPage.goToTrending();
-        trendingPage.listMenuTrendingMovies();
-        cy.wait(4000);
-        trendingPage.goToTrendingMovies();
-        //trendingPage.assertionPage();
-    });
-
-    // it.skip('Order Flight', () => {
-    //     cy.visit('https://www.agoda.com/id-id/');
-
-    //     // Click Flight Icon
-    //     cy.xpath("//li[@id='tab-flight-tab']").click();
-    //     cy.xpath("//input[@id='flight-origin-search-input']").type('Jakarta');
-    //     cy.xpath("//div[@class='Popup__content']//li[@data-selenium='autosuggest-item'][1]").click();
-    //     cy.xpath("//div[@class='AutocompleteSearch NewDesign'][2]").click();
-    //     cy.xpath("//input[@id='flight-destination-search-input']").type('Singapore');
-    //     cy.xpath("//div[@class='Popup__content']//li[@data-selenium='autosuggest-item'][1]").click();
-    //     cy.xpath(`//div[@class='PriceSurgePicker-Day PriceSurgePicker-Day__Wide '][@aria-label='Mon Jun 02 2025 ']`).click();
-    //     cy.xpath(`//button[@class='Buttonstyled__ButtonStyled-sc-5gjk6l-0 jyyvGo ChipItem Cabin ChipItem--selected'][@data-component='flight-search-cabinClass-Economy']`).click();
-    //     cy.xpath(`//div[@id='flight-occupancy']`).click();
-    //     cy.xpath(`//button[@data-selenium='searchButton']`).click();
-
-    //     // Select Maskapai
-    //     cy.xpath(`//div[@class='a5d86-box a5d86-fill-inherit a5d86-text-inherit a5d86-self-center a5d86-flex      ']//button[@type='button']`).click();
-    //     cy.xpath(`//div[@data-component='flight-filter-item-airline']//label[@data-element-value='Malaysia Airlines']//input[@type='checkbox']`).click();
-
-    //     // Sortir berdasarkan yang tercepat
-    //     cy.xpath(`//div[@data-element-name='flight-sort']`).click();
-    //     cy.xpath(`//div[@data-testid="floater-container"]//li[@role="presentation"][4]`).click();
-    //     cy.xpath(`//button[@type="button"]//div[@class="a5d86-box a5d86-fill-inherit a5d86-text-inherit a5d86-pr-4      "]//p[@id="sort-options-label"]`).should('be.visible').click();
-       
-    //     // Select flight from the list
-    //     cy.xpath(`//div[@class="sc-cjERFW fdGdnw"]//div[@class="sc-cCYyox iNqLle"][1]`).should('exist').click();
-    //     cy.xpath(`//div[@class="sc-cjERFW fdGdnw"]//div[@class="sc-cCYyox iNqLle"][1]`).should('be.visible');
-    //     //cy.xpath(`//button[@data-component="flight-card-bookButton"]`).should('exist');
-    //     // cy.xpath(`//button[@data-component="flight-card-bookButton"]`).click();
-
-    //     // Input Data
-    //     // cy.xpath(`//input[@id='contact.contactFirstName']`).type(Cypress.env('firstName'));
-    //     // cy.xpath(`//input[@id='contact.contactLastName']`).type(Cypress.env('lastName'));
-    //     // cy.xpath(`//input[@id='contact.contactEmail']`).type(Cypress.env('email'));
-    //     // cy.xpath(`//input[@id='contact.contactPhoneNumber']`).type(Cypress.env('phoneNumber'));
-
+    // it('youtube search', () => {
+    //     cy.visit(Cypress.env(`BASE_URL_YOUTUBE`));
+    //     cy.wait(4000);
+    //     trendingPage.goToTrending();
+    //     trendingPage.listMenuTrendingMovies();
+    //     cy.wait(4000);
+    //     trendingPage.goToTrendingMovies();
+    //     //trendingPage.assertionPage();
     // });
 
-    it('Search Chair on Amazon.com', () => {
-        cy.visit(Cypress.env('BASE_URL_AMAZON'));
-        cy.wait(5000);
-        filterItemPage.searchItems();
-        cy.wait(5000);
-        filterItemPage.sortFeatureItems();
-        filterItemPage.assertionPage();
+    it('Order Flight', () => {
+        cy.visit('https://www.agoda.com/id-id/');
+
+        // Click Flight Icon
+        cy.xpath("//li[@id='tab-flight-tab']").click();
+        cy.xpath("//input[@id='flight-origin-search-input']").type('Jakarta');
+        cy.xpath("//div[@class='Popup__content']//li[@data-selenium='autosuggest-item'][1]").click();
+        cy.xpath("//div[@class='AutocompleteSearch NewDesign'][2]").click();
+        cy.xpath("//input[@id='flight-destination-search-input']").type('Singapore');
+        cy.xpath("//div[@class='Popup__content']//li[@data-selenium='autosuggest-item'][1]").click();
+        cy.xpath(`//div[@class="PriceSurgePicker-Day__circle PriceSurgePicker-Day__circle--wide"]//span[@data-selenium-date="2025-06-04"]`).click();
+        cy.xpath(`//div[@class="ChipSetContainer"]//button[@data-component="flight-search-cabinClass-Economy"]`).click();
+        cy.xpath(`//div[@id="flight-occupancy"]//div[@class="Box-sc-kv6pi1-0 hRUYUu IconBox__wrapper"]`).click();
+        cy.xpath(`//button[@data-test="SearchButtonBox"]`).click();
+
+        // Select Maskapai
+        cy.xpath(`//button[@type="button"]//span[@label="Tampilkan semua 21 maskapai"]`).click();
+        // cy.xpath(`//div[@class='a5d86-box a5d86-fill-inherit a5d86-text-inherit a5d86-self-center a5d86-flex      ']//button[@type='button']`).click();
+        // cy.xpath(`//div[@data-component='flight-filter-item-airline']//label[@data-element-value='Malaysia Airlines']//input[@type='checkbox']`).click();
+
+        // // Sortir berdasarkan yang tercepat
+        // cy.xpath(`//div[@data-element-name='flight-sort']`).click();
+        // cy.xpath(`//div[@data-testid="floater-container"]//li[@role="presentation"][4]`).click();
+        // cy.xpath(`//button[@type="button"]//div[@class="a5d86-box a5d86-fill-inherit a5d86-text-inherit a5d86-pr-4      "]//p[@id="sort-options-label"]`).should('be.visible').click();
+       
+        // // Select flight from the list
+        // cy.xpath(`//div[@class="sc-cjERFW fdGdnw"]//div[@class="sc-cCYyox iNqLle"][1]`).should('exist').click();
+        // cy.xpath(`//div[@class="sc-cjERFW fdGdnw"]//div[@class="sc-cCYyox iNqLle"][1]`).should('be.visible');
+        //cy.xpath(`//button[@data-component="flight-card-bookButton"]`).should('exist');
+        // cy.xpath(`//button[@data-component="flight-card-bookButton"]`).click();
+
+        // Input Data
+        // cy.xpath(`//input[@id='contact.contactFirstName']`).type(Cypress.env('firstName'));
+        // cy.xpath(`//input[@id='contact.contactLastName']`).type(Cypress.env('lastName'));
+        // cy.xpath(`//input[@id='contact.contactEmail']`).type(Cypress.env('email'));
+        // cy.xpath(`//input[@id='contact.contactPhoneNumber']`).type(Cypress.env('phoneNumber'));
+
     });
+
+    // it('Search Chair on Amazon.com', () => {
+    //     cy.visit(Cypress.env('BASE_URL_AMAZON'));
+    //     cy.wait(5000);
+    //     filterItemPage.searchItems();
+    //     cy.wait(5000);
+    //     filterItemPage.sortFeatureItems();
+    //     filterItemPage.assertionPage();
+    // });
 
 });
